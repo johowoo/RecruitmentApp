3920e0f6985089cd4110b6a8d34debb816d7a541
johowoo <316488922@qq.com>
2019-03-22 17:42:11 +1300

    tmp

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,28 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+// const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 
-module.exports = function override(config, env) {
-    config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: true }], config);
-    config = rewireLess.withLoaderOptions({
-        modifyVars: { "@brand-primary": "#cc6699" }
-    })(config, env);
-    return config;
+const {
+    addLessLoader,
+    fixBabelImports,
+    override
+} = require("customize-cra");
+
+module.exports = {
+    webpack: override(
+        addLessLoader({
+            javascriptEnabled: true
+        }),
+        fixBabelImports("babel-plugin-import", {
+            libraryName: "antd-mobile",
+            style: true
+        })
+    )
 };
+
+// module.exports = function override(config, env) {
+//     config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: true }], config);
+//     config = rewireLess.withLoaderOptions({
+//         modifyVars: { "@brand-primary": "#cc6699" }
+//     })(config, env);
+//     return config;
+// };
